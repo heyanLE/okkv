a8249dd02cc8d557abd384251df816be01cc8c7c
heyanLe <1371735400@qq.com>
2021-11-24 23:29:00 +0800

    1.0.8

---
+++ app/src/main/java/com/heyanle/okkv/OkkvApplication.kt
@@ -0,0 +1,14 @@
+package com.heyanle.okkv
+
+import android.app.Application
+
+/**
+ * Created by HeYanLe on 2021/11/24 23:23.
+ * https://github.com/heyanLE
+ */
+class OkkvApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
